d78da87f0df5d9240bf5cf72c3cc11f635426f9f
Sakari Ellonen <sakari.ellonen@gmail.com>
2014-02-19 16:42:04 +0200

    english part almost finished

--- urls.py
+++ urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns as patterns
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    (r'^', TemplateView.as_view(template_name="index.html")),
-    (r'^what-makes-it-different/', TemplateView.as_view(template_name="what-makes-it-different.html")),
-    (r'^why-maps/', TemplateView.as_view(template_name="why-maps.html")),
-    (r'^our-customers/', TemplateView.as_view(template_name="our-customers.html")),
-    (r'^about-us/', TemplateView.as_view(template_name="about-us.html")),
+    (r'^$', TemplateView.as_view(template_name="index.html")),
+    (r'^what-makes-it-different/$', TemplateView.as_view(template_name="what-makes-it-different.html")),
+    (r'^products-and-services/$', TemplateView.as_view(template_name="products-and-services.html")),
+    (r'^our-customers/$', TemplateView.as_view(template_name="our-customers.html")),
+    (r'^about-us/$', TemplateView.as_view(template_name="about-us.html")),
 )
