b39514b12aaefd349b72e2ffef5b16777692e9b9
Mikko Johansson <mikko.johansson@mapita.fi>
2014-06-02 15:33:11 +0300

    first project cards added

--- urls.py
+++ urls.py
@@ -5,7 +5,8 @@ from django.views.generic import TemplateView
 urlpatterns = patterns('',
     (r'^$', TemplateView.as_view(template_name="index.html")),
     (r'^what-makes-it-different/$', TemplateView.as_view(template_name="what-makes-it-different.html")),
-    (r'^products-and-services/$', TemplateView.as_view(template_name="products-and-services.html")),
-    (r'^our-customers/$', TemplateView.as_view(template_name="our-customers.html")),
-    (r'^about-us/$', TemplateView.as_view(template_name="about-us.html")),
+    (r'^(?P<products>products-and-services)/$', TemplateView.as_view(template_name="products-and-services.html")),
+    (r'^(?P<customers>our-customers)/$', TemplateView.as_view(template_name="our-customers.html")),
+    (r'^(?P<people>about-us)/$', TemplateView.as_view(template_name="about-us.html")),
+    (r'^(?P<project>project-cards)/$', TemplateView.as_view(template_name="project-cards.html")),
 )
